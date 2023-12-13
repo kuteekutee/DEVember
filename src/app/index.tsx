51ffb4d9b614cde6891ef1a563923039ab94eecd
kuteekutee <bernardsng@gmail.com>
2023-12-13 13:15:29 +0800

    ts path aliases

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
-import DayListItem from '../../src/components/core/DayListItem';
+import DayListItem from '@components/core/DayListItem';
 
 const days = [...Array(24)].map((val, index) => index + 1)
 
 export default function HomeScreen() {
-
-
   return (
     <View style={styles.container}>
 
